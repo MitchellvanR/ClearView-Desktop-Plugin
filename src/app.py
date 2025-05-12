5a776a41e176e74c234e402952a6f2be9c3f3670
Mitchell van Rijswijk <mitchell.van.rijswijk@gmail.com>
2025-05-12 15:08:24 +0200

    Remove old code and rename source folder

--- src/app.py
+++ src/app.py
@@ -1,32 +1,33 @@
 import sys
+import threading
 import keyboard
-from PyQt5.QtCore import QTimer
+from functools import partial
 from PyQt5.QtWidgets import QApplication
-from list_ui import MainWindow
-
+from PyQt5.QtCore import QTimer
+from window_manager import WindowManager
 
 class MyApp(QApplication):
     def __init__(self, sys_argv):
         super().__init__(sys_argv)
-        self.window = MainWindow(self)
-        self.listen_for_shortcuts()
+        self.manager = WindowManager(self)
+        self.setup_global_hotkeys()
 
-    def listen_for_shortcuts(self):
-        """Listen for global hotkeys in a non-blocking way."""
-        keyboard.add_hotkey('alt+l', self.toggle_window)
+    def setup_global_hotkeys(self):
+        """Listen for global hotkeys in a separate thread."""
+        def hotkey_thread():
+            keyboard.add_hotkey('alt+l', partial(self.handle_hotkey, 'list'))
+            keyboard.add_hotkey('alt+n', partial(self.handle_hotkey, 'notepad'))
+            keyboard.add_hotkey('alt+c', partial(self.handle_hotkey, 'calculator'))
+            keyboard.wait()
 
-    def toggle_window(self):
-        """Schedule the window toggle in the main thread."""
-        QTimer.singleShot(0, self._toggle_window)
+        threading.Thread(target=hotkey_thread, daemon=True).start()
 
-    def _toggle_window(self):
-        """Toggle the window's visibility."""
-        if self.window.isVisible():
-            self.window.hide()
-        else:
-            self.window.show()
+    def handle_hotkey(self, window_type):
+        QTimer.singleShot(0, partial(self.toggle_correct_window, window_type))
 
-app = MyApp(sys.argv)
+    def toggle_correct_window(self, window_type):
+        self.manager.toggle_window(window_type)
 
-sys.exit(app.exec())
-
+if __name__ == "__main__":
+    app = MyApp(sys.argv)
+    sys.exit(app.exec())
--- src_new/list/models.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class ListItem:
-    text: str
-    completed: bool = False
-
